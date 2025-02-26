9e1d7184bd6f97612403dea7b9b67277205c465c
nailaaisyah <nailaaisyahrinaldi@gmail.com>
2025-02-26 14:30:50 +0700

    dashboard dan login

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/dashboard', function () {
+    return view('index');
+});
+
+Route::get('/halaman_login', function () {
+    return view('login');
+});
